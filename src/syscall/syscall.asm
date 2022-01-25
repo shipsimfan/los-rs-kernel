6f797031f452f3869db31a521a749c9269fe2f39
Lance Hart <shipsimfan@hotmail.com>
2022-01-24 22:38:45 -0500

    Fixed system call scratching function parameters

--- src/syscall/syscall.asm
+++ src/syscall/syscall.asm
@@ -11,24 +11,27 @@ GLOBAL system_call_handler
 system_call_handler:
     ; Get kernel stack
     mov rax, CURRENT_KERNEL_STACK
-    mov rdx, rsp ; Prepare to save user stack pointer
+    mov r12, rsp ; Prepare to save user stack pointer
     mov rsp, [rax] ; Switch stacks
 
+    sti
+
     ; Save required registers
-    push rdx ; User stack pointer
+    push r12 ; User stack pointer
     push rcx ; Return address
     push r11 ; User flags
-    
-    sti
+
+    ; Retrieve moved parameter
+    mov rcx, r10
 
     ; Perform Syscall
     call system_call
 
-    cli
-
     ; Retrieve required registers
     pop r11 ; User flags
     pop rcx ; Return address
+
+    cli
 
     ; Get user stack
     pop rsp
