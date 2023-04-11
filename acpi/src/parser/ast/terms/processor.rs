6be9071c965a22bbe9885d0a69242470276d86ec
Lance Hart <shipsimfan@hotmail.com>
2023-04-11 16:45:11 -0400

    Added processor

---
+++ acpi/src/parser/ast/terms/processor.rs
@@ -0,0 +1,56 @@
+use super::TermList;
+use crate::{
+    display_prefix, impl_core_display_lifetime,
+    parser::{name_string, next, pkg_length, Context, Result, Stream},
+    Display, Path,
+};
+
+pub(crate) struct Processor<'a> {
+    path: Path,
+    id: u8,
+    address: u32,
+    length: u8,
+    term_list: TermList<'a>,
+}
+
+impl<'a> Processor<'a> {
+    pub(super) fn parse(stream: &mut Stream<'a>, context: &mut Context) -> Result<Self> {
+        let mut stream = pkg_length::parse_to_stream(stream, "Processor")?;
+
+        let path = name_string::parse(&mut stream, "Processor")?;
+        let id = next!(stream, "Processor");
+        let address = u32::from_le_bytes([
+            next!(stream, "Processor"),
+            next!(stream, "Processor"),
+            next!(stream, "Processor"),
+            next!(stream, "Processor"),
+        ]);
+        let length = next!(stream, "Processor");
+
+        context.push_path(&path);
+        let term_list = TermList::parse(&mut stream, context)?;
+        context.pop_path();
+
+        Ok(Processor {
+            path,
+            id,
+            address,
+            length,
+            term_list,
+        })
+    }
+}
+
+impl<'a> Display for Processor<'a> {
+    fn display(&self, f: &mut core::fmt::Formatter, depth: usize, last: bool) -> core::fmt::Result {
+        display_prefix!(f, depth);
+        write!(
+            f,
+            "Processor ({}, {:#04X}, {:#010X}, {:#04X}) ",
+            self.path, self.id, self.address, self.length
+        )?;
+        self.term_list.display(f, depth, last)
+    }
+}
+
+impl_core_display_lifetime!(Processor);
--- acpi/src/parser/ast/terms/term.rs
+++ acpi/src/parser/ast/terms/term.rs
@@ -1,4 +1,4 @@
-use super::{Device, Field, Method, Mutex, Name, OpRegion, Scope};
+use super::{Device, Field, Method, Mutex, Name, OpRegion, Processor, Scope};
 use crate::{
     impl_core_display_lifetime,
     parser::{match_next, Context, Result, Stream},
@@ -12,6 +12,7 @@ pub(crate) enum Term<'a> {
     Mutex(Mutex),
     Name(Name<'a>),
     OpRegion(OpRegion<'a>),
+    Processor(Processor<'a>),
     Scope(Scope<'a>),
 }
 
@@ -25,6 +26,7 @@ const MUTEX_OP: u8 = 0x01;
 const OP_REGION_OP: u8 = 0x80;
 const FIELD_OP: u8 = 0x81;
 const DEVICE_OP: u8 = 0x82;
+const PROCESSOR_OP: u8 = 0x83;
 
 impl<'a> Term<'a> {
     pub(super) fn parse(stream: &mut Stream<'a>, context: &mut Context) -> Result<Self> {
@@ -37,6 +39,7 @@ impl<'a> Term<'a> {
                 FIELD_OP => Field::parse(stream).map(|field| Term::Field(field)),
                 MUTEX_OP => Mutex::parse(stream).map(|mutex| Term::Mutex(mutex)),
                 OP_REGION_OP => OpRegion::parse(stream, context).map(|op_region| Term::OpRegion(op_region)),
+                PROCESSOR_OP => Processor::parse(stream, context).map(|processor| Term::Processor(processor)),
             ),
         )
     }
@@ -51,6 +54,7 @@ impl<'a> Display for Term<'a> {
             Term::Mutex(mutex) => mutex.display(f, depth, last),
             Term::Name(name) => name.display(f, depth, last),
             Term::OpRegion(op_region) => op_region.display(f, depth, last),
+            Term::Processor(processor) => processor.display(f, depth, last),
             Term::Scope(scope) => scope.display(f, depth, last),
         }
     }
