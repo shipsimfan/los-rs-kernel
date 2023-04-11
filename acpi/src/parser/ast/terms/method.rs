24775538de876cb6ee3de26b4c351cfcd60211bc
Lance Hart <shipsimfan@hotmail.com>
2023-04-11 17:24:22 -0400

    Started parsing method term list

--- acpi/src/parser/ast/terms/method.rs
+++ acpi/src/parser/ast/terms/method.rs
@@ -1,17 +1,16 @@
+use super::TermList;
 use crate::{
     display_prefix, impl_core_display_lifetime,
     parser::{name_string, next, pkg_length, Context, Result, Stream},
     Display, Path,
 };
-use core::marker::PhantomData;
 
 pub(crate) struct Method<'a> {
     path: Path,
     argument_count: u8,
     serialized: bool,
     sync_level: u8,
-    // TODO: Add TermList
-    phantom: PhantomData<&'a ()>,
+    term_list: TermList<'a>,
 }
 
 fn parse_method_flags(flags: u8) -> (u8, bool, u8) {
@@ -32,24 +31,29 @@ impl<'a> Method<'a> {
 
         context.add_method(&path, argument_count, offset)?;
 
+        context.push_path(&path);
+        let term_list = TermList::parse(&mut stream, context)?;
+        context.pop_path();
+
         Ok(Method {
             path,
             argument_count,
             serialized,
             sync_level,
-            phantom: PhantomData,
+            term_list,
         })
     }
 }
 
 impl<'a> Display for Method<'a> {
-    fn display(&self, f: &mut core::fmt::Formatter, depth: usize, _: bool) -> core::fmt::Result {
+    fn display(&self, f: &mut core::fmt::Formatter, depth: usize, last: bool) -> core::fmt::Result {
         display_prefix!(f, depth);
-        writeln!(
+        write!(
             f,
-            "Method ({}, {}, {}, {})",
+            "Method ({}, {}, {}, {}) ",
             self.path, self.argument_count, self.serialized, self.sync_level
-        )
+        )?;
+        self.term_list.display(f, depth, last)
     }
 }
 
